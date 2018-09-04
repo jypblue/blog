4409bee15d9b093d45c2782a15640c592c3aad00
wangzhaoxiong <wangzhaoxiong@yit.com>
2018-09-04 11:45:13 +0800

    add blog project by change from vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  dest: 'jypblue',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'JYPBlue',
+      description: '我的笔记'
+    }
+  },
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
+  serviceWorker: true,
+  theme: 'vue',
+  themeConfig: {
+    repo: 'jypblue/blog',
+    editLinks: true,
+    docsDir: 'docs',
+    locales: {
+      '/': {
+        label: '简体中文',
+        selectText: '选择语言',
+        editLinkText: '编辑此页',
+        lastUpdated: '上次更新',
+        nav: [
+          {
+            text: '笔记',
+            link: '/guide/',
+          },
+          // {
+          //   text: '配置参考',
+          //   link: '/config/'
+          // },
+          // {
+          //   text: '默认主题配置',
+          //   link: '/default-theme-config/'
+          // }
+        ],
+        sidebar: {
+          '/guide/': genSidebarConfig('笔记')
+        }
+      }
+    }
+  }
+}
+
+function genSidebarConfig (title) {
+  return [
+    {
+      title,
+      collapsable: false,
+      children: [
+        '',
+        'getting-started',
+        'basic-config',
+        'assets',
+        'markdown',
+        'using-vue',
+        'custom-themes',
+        'i18n',
+        'deploy'
+      ]
+    }
+  ]
+}
---
+++ scripts/deploy-gh.sh
@@ -0,0 +1,5 @@
+cd blog
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:jypblue/blog.git master:gh-pages
